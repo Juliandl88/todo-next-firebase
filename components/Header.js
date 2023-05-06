00024461509289642d69ea77a280e4a6dec4b4ec
Julian <julian_delisio@hotmail.com>
2023-05-05 21:39:12 -0300

    se agrega modal

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from 'react'
+import Modal from './Modal'
 
 export default function Header() {
-  return (
-    <div className="sticky top-0 w-full left-0 bg-slate-900 text-white flex items-center 
-    justify-between p-4 border-b border-solid border-white">
-      <h2 className="text-3xl sm:text-6xl select-none">TODO LIST</h2>
-      <i class="fa-solid fa-user text-xl duration-300 hover:opacity-40 hover:scale-125 select-none cursor-pointer sm:text-3xl"></i>
-    </div>
-  );
+    const [openModal, setOpenModal] = useState(false)
+
+    return (
+        <>
+            {openModal && <Modal setOpenModal={setOpenModal} />}
+            <div className='sticky top-0 w-full left-0 bg-inherit flex items-center justify-between p-4 border-b border-solid border-white'>
+                <h1 className='text-3xl select-none sm:text-6xl'>TODO LIST</h1>
+                <i onClick={() => setOpenModal(true)} className="fa-solid fa-user text-xl duration-300 hover:opacity-40 cursor-pointer sm:text-3xl"></i>
+            </div>
+        </>
+    )
 }
