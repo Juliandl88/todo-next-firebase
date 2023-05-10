a716244bb6ec9ec5f3f0ead3d228b27ff5af789a
Julian <julian_delisio@hotmail.com>
2023-05-10 19:54:31 -0300

    se agrega card de todo

--- components/Header.js
+++ components/Header.js
@@ -6,7 +6,6 @@ export default function Header() {
 
     return (
         <>
-        HEADER APP REACT BOOTSTRAP and REDUX
             {openModal && <Modal setOpenModal={setOpenModal} />}
             <div className='sticky top-0 w-full left-0 bg-inherit flex items-center justify-between p-4 border-b border-solid border-white'>
                 <h1 className='text-3xl select-none sm:text-6xl'>TODO LIST</h1>
