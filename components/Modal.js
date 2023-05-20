a18b9096e58be502aaec7bff87695b34caf4ecba
Julian <julian_delisio@hotmail.com>
2023-05-19 22:16:51 -0300

    se agrega card de todo

--- components/Modal.js
+++ components/Modal.js
@@ -16,14 +16,14 @@ export default function Modal(props) {
     return ReactDom.createPortal(
         <div className='fixed inset-0 bg-white text-slate-900 text-lg sm:text-xl flex flex-col'>
             <div className='flex items-center justify-between border-b border-solid border-slate-900 p-4'>
-                <h1 className='font-extrabold text-2xl sm:text-5xl select-none'>MENU</h1>
+                <h1 className='font-extrabold text-2xl sm:text-5xl select-none'>MENU APP</h1>
                 <i onClick={() => setOpenModal(false)} className="fa-solid fa-xmark duration-300 hover:rotate-90 text-lg sm:text-3xl cursor-pointer"></i>
             </div>
             <div className='p-4 flex flex-col gap-3'>
                 <h2 onClick={() => {
                     logout()
                     setOpenModal(false)
-                }} className='select-none duration-300 hover:pl-2 cursor-pointer'>Log-Out</h2>
+                }} className='select-none duration-300 hover:pl-2 cursor-pointer'>LOGOUT</h2>
             </div>
         </div>,
         _document.getElementById('portal')
